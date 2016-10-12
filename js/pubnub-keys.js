f3ce60bfbd9b383a3d1e9746e965541df67c0405
Sang Won Lee <snaglee@umich.edu>
2016-10-11 20:21:40 -0400

    http --> https

---
+++ js/pubnub-keys.js
@@ -0,0 +1,5 @@
+
+// this is snaglee@umich.edu
+// this is demo key so no one is paying it. 
+publishKey ="pub-c-90ec0e12-8375-42a7-8315-15bf68f7ab04";
+subscribeKey = "sub-c-60152074-fb22-11e5-8916-0619f8945a4f";
